e5c98459c5a286f08423dd31223a6331222c0bde
JT <jtjobe@gmail.com>
2015-02-26 17:10:21 -0500

    Redesign #1

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,14 @@
 $(window).scroll(function() {
 if ($(this).scrollTop() > 10){  
-    $('header').addClass("sticky");
-    // $('.full-logo').addClass("hidden");
-    $('nav div').addClass("small-nav");
+    $('header').addClass("sticky-header");
+    $('.full-logo').addClass("sticky-logo");
+    $('nav').addClass("sticky-nav");
+    $('.nav-button').addClass("sticky-nav-button");
   }
   else{
-    $('header').removeClass("sticky");
-    // $('.full-logo').removeClass("hidden");
-    $('nav div').removeClass("small-nav");
+    $('header').removeClass("sticky-header");
+    $('.full-logo').removeClass("sticky-logo");
+    $('nav').removeClass("sticky-nav");
+    $('.nav-button').removeClass("sticky-nav-button");
   }
 });
