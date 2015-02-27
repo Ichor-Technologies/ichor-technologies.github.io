993800b21871c4efb8a440c0083e5b2a369af542
JT <jtjobe@gmail.com>
2015-02-27 14:13:10 -0500

    Remove re-sizing nav for small view (janky fix - idential CSS classes for regular and sticky counterparts until MQ > 900px

--- js/custom.js
+++ js/custom.js
@@ -3,14 +3,14 @@ if ($(this).scrollTop() > 10){
     $('header').addClass("sticky-header");
     $('.full-logo').addClass("sticky-logo");
     $('nav').addClass("sticky-nav");
-    $('#dropdown-menu').addClass("sticky-dropdown");
     $('.nav-button').addClass("sticky-nav-button");
   }
   else{
     $('header').removeClass("sticky-header");
     $('.full-logo').removeClass("sticky-logo");
     $('nav').removeClass("sticky-nav");
-    $('#dropdown-menu').removeClass("sticky-dropdown");
     $('.nav-button').removeClass("sticky-nav-button");
   }
 });
+
+
