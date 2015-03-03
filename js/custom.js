caf91b128524175ce0712a5c613779bc48a0e94c
JT <jtjobe@gmail.com>
2015-03-03 11:16:15 -0500

    Add hidden link (on copyright notice) to alternate page with video at top of page

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,17 @@
 $(window).scroll(function() {
 if ($(this).scrollTop() > 10){  
-    $('header').addClass("sticky-header");
-    $('.full-logo').addClass("sticky-logo");
-    $('nav').addClass("sticky-nav");
-    $('.nav-button').addClass("sticky-nav-button");
+    $('header').addClass('sticky-header');
+    $('.full-logo').addClass('sticky-logo');
+    $('nav').addClass('sticky-nav');
+    $('.nav-button').addClass('sticky-nav-button');
+    $('.video-container').addClass('sticky-video-container')
   }
   else{
-    $('header').removeClass("sticky-header");
-    $('.full-logo').removeClass("sticky-logo");
-    $('nav').removeClass("sticky-nav");
-    $('.nav-button').removeClass("sticky-nav-button");
+    $('header').removeClass('sticky-header');
+    $('.full-logo').removeClass('sticky-logo');
+    $('nav').removeClass('sticky-nav');
+    $('.nav-button').removeClass('sticky-nav-button');
+    $('.video-container').removeClass('sticky-video-container')
   }
 });
 
